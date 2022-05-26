a99daf157ef8c71aa0357e7daaa3ce81f9677f4c
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2022-05-26 16:53:28 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,24 @@ function take_snapshot(){
 }
 
 console.log('ml5 version:', ml5.version)
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7EMy5RqSk/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Z7k-yD8ee/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded');
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error)        
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
